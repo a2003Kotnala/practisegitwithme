f6bb8efa133f496e99477ad89ae965e2e25c6687
Ankit Kotnala <68368469+a2003Kotnala@users.noreply.github.com>
2025-01-30 06:18:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 def addition(a,b):
     return a + b
 
-def subtraction(a,b):
-    return a - b
+def subtraction(a,b,c):
+    return a - b - c
 
 def division(a,b):
     return a/b
 
-def multiplication(a,b):
-    return a*b
+def multiplication(a,b,d):
+    return a*b*d
