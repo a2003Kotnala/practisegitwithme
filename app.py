e0b767bcf0050c94425691f29eea0d6dbd9a0197
Ankit Kotnala <ankit.kotnala12@gmail.com>
2025-01-30 06:19:48 +0530

    Added more variables

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 def addition(a,b):
     return a + b
 
-def subtraction(a,b):
-    return a - b
+def subtraction(a,b,g):
+    return a - b - g
 
 def division(a,b):
     return a/b
 
-def multiplication(a,b):
-    return a*b
+def multiplication(a,b,j):
+    return a*b*j
